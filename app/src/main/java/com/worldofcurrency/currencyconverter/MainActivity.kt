3b875ab4cac8831ec83f0e04d6523f4ec388913b
vdunaev <dunvladyur4@gmail.com>
2021-12-10 16:39:19 +0300

    Added elements for currency conversion

--- app/src/main/java/com/worldofcurrency/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/worldofcurrency/currencyconverter/MainActivity.kt
@@ -2,7 +2,8 @@ package com.worldofcurrency.currencyconverter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.view.View
+import android.widget.*
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
@@ -15,27 +16,63 @@ import org.json.JSONArray
 class MainActivity : AppCompatActivity() {
 
     private var requestQueue: RequestQueue? = null
+    private var spinner1: Spinner? = null
+    private var spinner2: Spinner? = null
+    private var input: EditText? = null
+    private var output: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textView = findViewById<TextView>(R.id.text)
+        spinner1 = findViewById(R.id.first_currency_spinner)
+        spinner2 = findViewById(R.id.second_currency_spinner)
+        input = findViewById(R.id.currency_input)
+        output = findViewById(R.id.currency_output)
         requestQueue = Volley.newRequestQueue(this)
-        val url = "https://api.fastforex.io/currencies?api_key=demo"
 
-        val stringRequest = JsonObjectRequest(Request.Method.GET, url, null,
+        val getCurrencyRequest = JsonObjectRequest(
+            Request.Method.GET,
+            "https://api.fastforex.io/currencies?api_key=demo",
+            null,
             { jsonResponse ->
-                val currenciesMap = jsonResponse.getJSONObject("currencies")
-                var text = ""
-                for (key in currenciesMap.keys()) {
-                    val value = currenciesMap.get(key).toString()
-                    text += "$key -> $value\n"
-                }
-                textView.text = text
-            },
-            { textView.text = "That didn't work!" })
 
-        requestQueue?.add(stringRequest)
+                val currencyList =
+                    jsonResponse.getJSONObject("currencies").keys().asSequence().toMutableList()
+                currencyList.remove("EUR")
+                currencyList.add(0, "EUR")
+                currencyList.remove("USD")
+                currencyList.add(0, "USD")
+                currencyList.remove("RUB")
+                currencyList.add(0, "RUB")
+
+                val adapter = ArrayAdapter(
+                    this,
+                    android.R.layout.simple_spinner_item,
+                    currencyList.toTypedArray()
+                )
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                spinner1?.adapter = adapter
+                spinner2?.adapter = adapter
+            },
+            { })
+
+        requestQueue?.add(getCurrencyRequest)
+
+        val button = findViewById<Button>(R.id.button_convert)
+        button.setOnClickListener {
+            val amount = input?.text.toString().toDouble()
+            val from = spinner1?.selectedItem.toString()
+            val to = spinner2?.selectedItem.toString()
+            val convertRequest = JsonObjectRequest(
+                Request.Method.GET,
+                "https://api.fastforex.io/convert?from=$from&to=$to&amount=$amount&api_key=demo",
+                null,
+                { jsonResponse ->
+                    output?.text = jsonResponse.getJSONObject("result").getString(to)
+                },
+                { })
+            requestQueue?.add(convertRequest)
+        }
     }
 }
